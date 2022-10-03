11a3baa97e6c7e6f6bc373988206c6d380b3c409
Wei Jie <jackchong398@gmail.com>
2022-10-03 23:25:54 +0800

    Make a User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :students
   root 'home#index'
-  get 'home/about'
+  get '/about', to: 'home#about'
+  get '/signup', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
